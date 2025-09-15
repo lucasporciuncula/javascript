68daa460fd0706453bf2f38ef555c7e1d1ad4adf
Eojaoo1 <joao.v.menezes24@aluno.senai.br>
2025-09-15 15:34:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.get("/João",(req,res) => {
+    nome = "João"
+    console.log(nome)
+    res.render("João", (nome))
+})
+
 app.listen(3000,() => {
     console.log("Servidor rodando em http://localhost:3000")
 })
