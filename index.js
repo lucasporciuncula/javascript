686d5c27e024c64dc1c60557f18c47c313427445
lucasporciuncula <l.porciuncula@aluno.senai.br>
2025-09-15 15:38:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require ('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,12 +6,7 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/João",(req,res) => {
-    nome = "João"
-    console.log(nome)
-    res.render("João", (nome))
-})
-
 app.listen(3000,() => {
     console.log("Servidor rodando em http://localhost:3000")
 })
+
